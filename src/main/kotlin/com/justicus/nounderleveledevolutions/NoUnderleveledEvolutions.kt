7aa5472da7ad84a397b7e121fd9b2d07bdc95e07
nmneve <40474860+nmneve@users.noreply.github.com>
2025-07-11 15:52:58 -0500

    Cleaned up unused code

--- src/main/kotlin/com/justicus/nounderleveledevolutions/NoUnderleveledEvolutions.kt
+++ src/main/kotlin/com/justicus/nounderleveledevolutions/NoUnderleveledEvolutions.kt
@@ -1,16 +1,9 @@
 package com.justicus.nounderleveledevolutions
 
 import com.cobblemon.mod.common.api.events.CobblemonEvents
-import com.cobblemon.mod.common.api.pokemon.PokemonProperties
 import com.cobblemon.mod.common.pokemon.evolution.requirements.LevelRequirement
-import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
-import com.cobblemon.mod.common.pokemon.Pokemon
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
-import net.minecraft.client.MinecraftClient
-import net.minecraft.entity.Entity
-import net.minecraft.server.world.ServerWorld
 import org.slf4j.LoggerFactory
 import java.util.UUID
 
@@ -24,17 +17,12 @@ object NoUnderleveledEvolutions : ModInitializer {
 	override fun onInitialize() {
 		CobblemonEvents.POKEMON_ENTITY_SPAWN.subscribe { evt ->
 			logger.info("Spawn detected")
-//			evt.entity.discard()
-//			evt.entity.remove(Entity.RemovalReason.DISCARDED)
-//			toRemove.add(evt.entity.uuid)
-			//evt.cancel()
 			val pokemon = evt.entity.pokemon
 			val species = pokemon.species
 			val level = pokemon.level
 			val preEvo = pokemon.preEvolution
 
 			logger.info("SERVER: $species's pre-evolution is ${preEvo?.species}")
-			//logger.info("They evolve at ${(preEvo?.form?.evolutions?.first()?.requirements?.first() as LevelRequirement).minLevel}")
 
 			val evolutions = preEvo?.form?.evolutions
 			val levelRequirement = evolutions
@@ -60,34 +48,5 @@ object NoUnderleveledEvolutions : ModInitializer {
 				}
 			}
 		}
-
-//		ServerEntityEvents.ENTITY_LOAD.register { ent, world ->
-//			logger.info("SERVER: Spawn Detected")
-//
-//			if (ent is PokemonEntity) {
-//				val pokemon = ent.pokemon
-//				invalidEvolutionCheck(pokemon)
-//			}
-//		}
 	}
-
-//	fun invalidEvolutionCheck(pokemon: Pokemon) {
-//		val species = pokemon.species
-//		val level = pokemon.level
-//		val preEvo = pokemon.preEvolution
-//
-//		logger.info("SERVER: $species's pre-evolution is ${preEvo?.species}")
-//		//logger.info("They evolve at ${(preEvo?.form?.evolutions?.first()?.requirements?.first() as LevelRequirement).minLevel}")
-//
-//		val evolutions = preEvo?.form?.evolutions
-//		val levelRequirement = evolutions
-//			?.flatMap { it.requirements }
-//			?.filterIsInstance<LevelRequirement>()
-//			?.firstOrNull()
-//
-//		if (levelRequirement != null && level > levelRequirement.minLevel) {
-//			logger.info("SERVER: Invalid level. Deleting entity.")
-//			pokemon.entity?.discard()
-//		}
-//	}
 }
